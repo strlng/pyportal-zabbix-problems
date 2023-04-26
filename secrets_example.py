ce0b014c44d0ab31f571d3c3ea3eb50a764f47ea
Sterling Anderson <sterling.anderson@wisc.edu>
2023-04-26 15:29:14 +0000

    Added example secrets file.

---
+++ secrets_example.py
@@ -0,0 +1,12 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+# which would be not great. So, instead, keep it all in this one file and
+# keep it a secret.
+
+secrets = {
+    'ssid' : '',             # Keep the two '' quotes around the name
+    'password' : '',         # Keep the two '' quotes around password
+    'timezone' : "",  # http://worldtimeapi.org/timezones
+    'api_url' : "http://zabiix.server/api_jsonrpc.php",
+    'auth_key' : ""
+    }
